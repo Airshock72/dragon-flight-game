696a81accf868c1d4231667d2944ae0e65ce716b
gela.lomidze <gela.lomidze@iliauni.edu.ge>
2024-10-25 23:56:29 +0400

    separated dragon animation file

---
+++ src/components/DragonAnimation.tsx
@@ -0,0 +1,42 @@
+import {FC, useRef} from 'react'
+import * as PIXI from 'pixi.js'
+import {AnimatedSprite} from '@pixi/react'
+
+
+const DragonAnimation: FC = () => {
+    const dragonRef = useRef<PIXI.AnimatedSprite | null>(null)
+
+    const dragonBaseTexture = PIXI.BaseTexture.from('/assets/dragon_animation/Dragon_Fly.png')
+
+    const sheetWidth = 5840; // Total width of the sprite sheet
+    const sheetHeight = 5944; // Total height of the sprite sheet
+    const frameWidth = sheetWidth / 4; // Width of each frame in the sprite sheet
+    const frameHeight = sheetHeight / 8; // Height of each frame in the sprite sheet
+
+    // Prepare dragon animation frames
+    const dragonFrames = Array.from({ length: 31 }, (_, index) => {
+        const row = Math.floor(index / 4);
+        const col = index % 4;
+        return new PIXI.Texture(
+            dragonBaseTexture,
+            new PIXI.Rectangle(col * frameWidth, row * frameHeight, frameWidth, frameHeight)
+        );
+    });
+
+    return (
+        <AnimatedSprite
+            ref={dragonRef}
+            textures={dragonFrames}
+            isPlaying={true}
+            initialFrame={0}
+            animationSpeed={0.5}
+            x={0}
+            y={1500}
+            width={frameWidth * 0.5} // Reduce width to 50% of the original
+            height={frameHeight * 0.5} // Reduce height to 50% of the original
+            loop={true}
+        />
+    )
+}
+
+export default DragonAnimation
--- src/components/ParallaxBackground.tsx
+++ src/components/ParallaxBackground.tsx
@@ -1,6 +1,7 @@
 import {FC, MutableRefObject, useEffect, useRef} from 'react'
 import * as PIXI from 'pixi.js'
-import {AnimatedSprite, Sprite, Stage} from '@pixi/react'
+import {Sprite, Stage} from '@pixi/react'
+import DragonAnimation from './DragonAnimation.tsx'
 
 const ParallaxBackground: FC = () => {
 
@@ -9,26 +10,6 @@ const ParallaxBackground: FC = () => {
     const iceSprite2Ref = useRef<PIXI.Sprite | null>(null)
     const waySprite1Ref = useRef<PIXI.Sprite | null>(null)
     const waySprite2Ref = useRef<PIXI.Sprite | null>(null)
-    const dragonRef = useRef<PIXI.AnimatedSprite | null>(null)
-
-    const dragonBaseTexture = PIXI.BaseTexture.from('/assets/dragon_animation/Dragon_Fly.png')
-
-
-    const sheetWidth = 5840; // Total width of the sprite sheet
-    const sheetHeight = 5944; // Total height of the sprite sheet
-    const frameWidth = sheetWidth / 4; // Width of each frame in the sprite sheet
-    const frameHeight = sheetHeight / 8; // Height of each frame in the sprite sheet
-
-    // Prepare dragon animation frames
-    const dragonFrames = Array.from({ length: 31 }, (_, index) => {
-        const row = Math.floor(index / 4);
-        const col = index % 4;
-        return new PIXI.Texture(
-            dragonBaseTexture,
-            new PIXI.Rectangle(col * frameWidth, row * frameHeight, frameWidth, frameHeight)
-        );
-    });
-
 
     useEffect(() => {
         if (
@@ -36,7 +17,6 @@ const ParallaxBackground: FC = () => {
             || !waySprite1Ref.current
             || !waySprite2Ref.current
             || !iceSprite1Ref.current
-            || !iceSprite2Ref.current
         ) return
 
         const ticker = new PIXI.Ticker()
@@ -161,18 +141,7 @@ const ParallaxBackground: FC = () => {
                 anchor={{ x: 1, y: 0 }} // Anchor to the right side for flipping
                 scale={{ x: -1, y: 1 }} // Flip horizontally using scale
             />
-            <AnimatedSprite
-                ref={dragonRef}
-                textures={dragonFrames}
-                isPlaying={true}
-                initialFrame={0}
-                animationSpeed={0.5}
-                x={0}
-                y={1500}
-                width={frameWidth * 0.5} // Reduce width to 50% of the original
-                height={frameHeight * 0.5} // Reduce height to 50% of the original
-                loop={true}
-            />
+            <DragonAnimation />
             <Sprite
                 image="/assets/Uv.png"
                 x={0}
