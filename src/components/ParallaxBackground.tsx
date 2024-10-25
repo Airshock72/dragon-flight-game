502a8daef364c130dc1d7ce3c040a8d2f7a06667
gela.lomidze <gela.lomidze@iliauni.edu.ge>
2024-10-25 23:50:34 +0400

    added dragon with animation

--- src/components/ParallaxBackground.tsx
+++ src/components/ParallaxBackground.tsx
@@ -1,6 +1,6 @@
 import {FC, MutableRefObject, useEffect, useRef} from 'react'
 import * as PIXI from 'pixi.js'
-import {Sprite, Stage} from '@pixi/react'
+import {AnimatedSprite, Sprite, Stage} from '@pixi/react'
 
 const ParallaxBackground: FC = () => {
 
@@ -9,6 +9,26 @@ const ParallaxBackground: FC = () => {
     const iceSprite2Ref = useRef<PIXI.Sprite | null>(null)
     const waySprite1Ref = useRef<PIXI.Sprite | null>(null)
     const waySprite2Ref = useRef<PIXI.Sprite | null>(null)
+    const dragonRef = useRef<PIXI.AnimatedSprite | null>(null)
+
+    const dragonBaseTexture = PIXI.BaseTexture.from('/assets/dragon_animation/Dragon_Fly.png')
+
+
+    const sheetWidth = 5840; // Total width of the sprite sheet
+    const sheetHeight = 5944; // Total height of the sprite sheet
+    const frameWidth = sheetWidth / 4; // Width of each frame in the sprite sheet
+    const frameHeight = sheetHeight / 8; // Height of each frame in the sprite sheet
+
+    // Prepare dragon animation frames
+    const dragonFrames = Array.from({ length: 31 }, (_, index) => {
+        const row = Math.floor(index / 4);
+        const col = index % 4;
+        return new PIXI.Texture(
+            dragonBaseTexture,
+            new PIXI.Rectangle(col * frameWidth, row * frameHeight, frameWidth, frameHeight)
+        );
+    });
+
 
     useEffect(() => {
         if (
@@ -140,6 +160,18 @@ const ParallaxBackground: FC = () => {
                 height={500}
                 anchor={{ x: 1, y: 0 }} // Anchor to the right side for flipping
                 scale={{ x: -1, y: 1 }} // Flip horizontally using scale
+            />
+            <AnimatedSprite
+                ref={dragonRef}
+                textures={dragonFrames}
+                isPlaying={true}
+                initialFrame={0}
+                animationSpeed={0.5}
+                x={0}
+                y={1500}
+                width={frameWidth * 0.5} // Reduce width to 50% of the original
+                height={frameHeight * 0.5} // Reduce height to 50% of the original
+                loop={true}
             />
             <Sprite
                 image="/assets/Uv.png"
