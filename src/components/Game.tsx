008c0fcc92df8814ae1abbf3c1a5d90a42594c11
Airshock72 <gelagela2222@gmail.com>
2024-10-31 18:08:39 +0400

    separated BackgroundParallax component

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -8,8 +8,9 @@ import {TextStyle} from 'pixi.js'
 import SparklesAnimation from './SparklesAnimation.tsx'
 import LightningAnimation from './LightningAnimation.tsx'
 import useGame from '../hooks/useGame.ts'
+import BackgroundParallax from './BackgroundParallax.tsx'
 
-const ParallaxBackground = () => {
+const Game = () => {
     const {
         iceCubeRef,
         buttonTextRef,
@@ -48,72 +49,12 @@ const ParallaxBackground = () => {
 
     return (
         <Stage width={2160} height={3840}>
-            <Sprite
-                image="/assets/Background.png"
-                x={0}
-                y={0}
-                width={2160}
-                height={3840}
-            />
-            <Sprite
-                ref={iceSprite1Ref}
-                image="/assets/Background_Ice.png"
-                x={0}
-                y={0}
-                width={6000}
-                height={3840}
-            />
-            <Sprite
-                ref={iceSprite2Ref}
-                image="/assets/Background_Ice.png"
-                x={6000} // Positioned right after the first sprite
-                y={0}
-                width={6000}
-                height={3840}
-                anchor={{ x: 1, y: 0 }} // Anchor to the right side for flipping
-                scale={{ x: -1, y: 1 }} // Flip horizontally using scale
-            />
-            <Sprite
-                ref={waySprite1Ref}
-                image="/assets/Way.png"
-                x={0}
-                y={2715}
-                width={3500}
-                height={500}
-            />
-            <Sprite
-                ref={iceSpriteRefs.current[0]}
-                image="/assets/Ice_01.png"
-                x={0}
-                y={2300}
-                width={1000}
-                height={500}
-            />
-            <Sprite
-                ref={iceSpriteRefs.current[1]}
-                image="/assets/Ice_02.png"
-                x={3500}
-                y={2300}
-                width={1000}
-                height={500}
-            />
-            <Sprite
-                ref={iceSpriteRefs.current[2]}
-                image="/assets/Ice_03.png"
-                x={7000}
-                y={2300}
-                width={1000}
-                height={500}
-            />
-            <Sprite
-                ref={waySprite2Ref}
-                image="/assets/Way.png"
-                x={3500}
-                y={2715}
-                width={3500}
-                height={500}
-                anchor={{ x: 1, y: 0 }} // Anchor to the right side for flipping
-                scale={{ x: -1, y: 1 }} // Flip horizontally using scale
+            <BackgroundParallax
+                iceSprite1Ref={iceSprite1Ref}
+                iceSpriteRefs={iceSpriteRefs}
+                iceSprite2Ref={iceSprite2Ref}
+                waySprite1Ref={waySprite1Ref}
+                waySprite2Ref={waySprite2Ref}
             />
             <Graphics
                 ref={coinBackgroundRef}
@@ -334,4 +275,4 @@ const ParallaxBackground = () => {
     )
 }
 
-export default ParallaxBackground
+export default Game
